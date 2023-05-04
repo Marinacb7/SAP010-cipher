611cd41f2d454d33e4d6dc81d79c23a2b9a88b6a
Marinacb7 <marinacordeiro92@gmail.com>
2023-05-04 15:00:39 -0300

    Penúltimo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
 const cipher = {
-  // ...
+  encode: function(offset, string) {
+    let result = "";
+    for (let i = 0; i < string.length; i++) {
+      const charCode = string.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        const shiftedCharCode = ((charCode - 65 + offset) % 26) + 65;
+        result += String.fromCharCode(shiftedCharCode);
+      } else {
+        result += string.charAt(i);
+      }
+    }
+    return result;
+  },
+
+  decode: function(offset, string) {
+    return this.encode((26 - offset) % 26, string);
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
+const messageInput = document.getElementById('message-input');
+const offsetInput = document.getElementById('offset-input');
+const encodeButton = document.getElementById('encode-button');
+const decodeButton = document.getElementById('decode-button');
+const resultOutput = document.getElementById('result-output');
 
+encodeButton.addEventListener('click', function() {
+  const message = messageInput.value;
+  const offset = parseInt(offsetInput.value);
+  const encodedMessage = cipher.encode(offset, message);
+  resultOutput.value = encodedMessage;
+});
+
+decodeButton.addEventListener('click', function() {
+  const message = messageInput.value;
+  const offset = parseInt(offsetInput.value);
+  const decodedMessage = cipher.decode(offset, message);
+  resultOutput.value = decodedMessage;
+});
+   
 console.log(cipher);
