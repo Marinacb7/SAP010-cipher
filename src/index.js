0e1a0c2a6d850b233920c619814e95d0c69eb034
Marinacb7 <marinacordeiro92@gmail.com>
2023-05-04 21:48:16 -0300

    Comentários index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import cipher from './cipher.js';
+// Definição das variáveis
 const messageInput = document.getElementById('message-input');
 const offsetInput = document.getElementById('offset-input');
 const encodeButton = document.getElementById('encode-button');
 const decodeButton = document.getElementById('decode-button');
 const resultOutput = document.getElementById('result-output');
 
+// Chamar a função de encode ao botão
 encodeButton.addEventListener('click', function() {
   const message = messageInput.value;
   const offset = parseInt(offsetInput.value);
@@ -12,6 +14,7 @@ encodeButton.addEventListener('click', function() {
   resultOutput.value = encodedMessage;
 });
 
+// Chamar a função de decode ao botão
 decodeButton.addEventListener('click', function() {
   const message = messageInput.value;
   const offset = parseInt(offsetInput.value);
