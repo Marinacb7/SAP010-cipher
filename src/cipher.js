6587f94e46386bbbb1a2c8d5566196a5f97a496f
Marinacb7 <marinacordeiro92@gmail.com>
2023-05-17 23:21:41 -0300

    Alteração letras maiúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -6,14 +6,22 @@ const cipher = {
     if (typeof string !== 'string') {
       throw new TypeError('O segundo argumento deve ser uma string.');
     }
-    let result = "";
+  // Declaração de uma variável chamada "result" inicializada como uma string vazia
+    let result = ""; 
+  // Loop que percorre cada caractere da string
     for (let i = 0; i < string.length; i++) {
-      const char = string[i];
+  // Obtém o caractere na posição atual
+      const char = string[i].toUpperCase();
+  // Verifica se o caractere é uma letra maiúscula usando uma expressão regular
       if (/[A-Z]/.test(char)) {
+  // Obtém o código ASCII do caractere
         const charCode = char.charCodeAt(0);
+  // Realiza o deslocamento de acordo com o offset fornecido
         const shiftedCharCode = ((charCode - 65 + offset) % 26) + 65;
+  // Adiciona o caractere resultante ao resultado final
         result += String.fromCharCode(shiftedCharCode);
       } else {
+  // Adiciona o caractere original ao resultado fina
         result += char;
       }
     }
@@ -29,11 +37,14 @@ const cipher = {
     }
     let result = "";
     for (let i = 0; i < string.length; i++) {
-      const char = string[i];
+      const char = string[i].toUpperCase();
       if (/[A-Z]/.test(char)) {
         const charCode = char.charCodeAt(0);
+  // Realiza o deslocamento invertido de acordo com o offset fornecido
         const shiftedCharCode = charCode - 65 - offset;
+  // Realiza a decodificação aplicando o deslocamento invertido
         const decodedCharCode = ((shiftedCharCode % 26) + 26) % 26 + 65;
+  // Adiciona o caractere decodificado ao resultado final
         result += String.fromCharCode(decodedCharCode);
       } else {
         result += char;
