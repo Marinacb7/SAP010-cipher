2fc4acf5706cd7e1197b7ead0c6743cf1f00c20f
Marinacb7 <marinacordeiro92@gmail.com>
2023-06-13 22:34:47 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,27 +1,25 @@
 const cipher = {
   encode(offset, string) {
+    const ALPHABET_START = 65; // Valor ASCII da letra 'A'
+    const ALPHABET_SIZE = 26; // Tamanho do alfabeto
+
     if (typeof offset !== 'number') {
       throw new TypeError('O primeiro argumento deve ser um número.');
     }
     if (typeof string !== 'string') {
       throw new TypeError('O segundo argumento deve ser uma string.');
     }
-    // Declaração de uma variável chamada "result" inicializada como uma string vazia
-    let result = ""; 
-    // Loop que percorre cada caractere da string
+
+    let result = "";
     for (let i = 0; i < string.length; i++) {
-      // Obtém o caractere na posição atual
       const char = string[i].toUpperCase();
-      // Verifica se o caractere é uma letra maiúscula usando uma expressão regular
       if (/[A-Z]/.test(char)) {
-        // Obtém o código ASCII do caractere
         const charCode = char.charCodeAt(0);
-        // Realiza o deslocamento de acordo com o offset fornecido
-        const shiftedCharCode = ((charCode - 65 + offset) % 26) + 65;
-        // Adiciona o caractere resultante ao resultado final
-        result += String.fromCharCode(shiftedCharCode);
+        const normalizedCharCode = charCode - ALPHABET_START;
+        const shiftedCharCode = (normalizedCharCode + offset) % ALPHABET_SIZE;
+        const encodedCharCode = (shiftedCharCode + ALPHABET_START);
+        result += String.fromCharCode(encodedCharCode);
       } else {
-        // Adiciona o caractere original ao resultado fina
         result += char;
       }
     }
@@ -29,22 +27,24 @@ const cipher = {
   },
 
   decode(offset, string) {
+    const ALPHABET_START = 65; // Valor ASCII da letra 'A'
+    const ALPHABET_SIZE = 26; // Tamanho do alfabeto
+
     if (typeof offset !== 'number') {
       throw new TypeError('O primeiro argumento deve ser um número.');
     }
     if (typeof string !== 'string') {
       throw new TypeError('O segundo argumento deve ser uma string.');
     }
+
     let result = "";
     for (let i = 0; i < string.length; i++) {
       const char = string[i].toUpperCase();
       if (/[A-Z]/.test(char)) {
         const charCode = char.charCodeAt(0);
-        // Realiza o deslocamento invertido de acordo com o offset fornecido
-        const shiftedCharCode = charCode - 65 - offset;
-        // Realiza a decodificação aplicando o deslocamento invertido
-        const decodedCharCode = ((shiftedCharCode % 26) + 26) % 26 + 65;
-        // Adiciona o caractere decodificado ao resultado final
+        const normalizedCharCode = charCode - ALPHABET_START;
+        const shiftedCharCode = (normalizedCharCode - offset);
+        const decodedCharCode = ((shiftedCharCode % ALPHABET_SIZE) + ALPHABET_SIZE) % ALPHABET_SIZE + ALPHABET_START;
         result += String.fromCharCode(decodedCharCode);
       } else {
         result += char;
@@ -55,4 +55,5 @@ const cipher = {
 };
 
 export default cipher;
+
 
